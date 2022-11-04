59bd6d94cc35f9b42b00c092022e61c2c3c35a71
afantiniv <fantiniv95@gmail.com>
2022-11-04 02:06:24 +0000

    Generar onclick listo

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ let when = [
 
 /*
 function generator() {
-  let x = who[Math.floor(Math.random() * who.length)];
+/workspace/vanillajs-hello/node_modules  let x = who[Math.floor(Math.random() * who.length)];
   let w = action[Math.floor(Math.random() * action.length)];
   let y = what[Math.floor(Math.random() * what.length)];
   let z = when[Math.floor(Math.random() * when.length)];
@@ -30,14 +30,21 @@ function aleatorio(arreglo) {
   return x;
 }
 
-let excusa = "";
-
-[who, action, when, what].forEach(parte => {
-  excusa = excusa + aleatorio(parte) + " ";
-});
-
-window.onload = function() {
+/*window.onload = function() {
   //write your code here
   document.getElementById("excuse").innerHTML = excusa;
   console.log("Look Mom! I'm on the internet!");
+};*/
+
+function generar() {
+  let excusa = "";
+  [who, action, when, what].forEach(parte => {
+    excusa = excusa + aleatorio(parte) + " ";
+  });
+  document.getElementById("excuse").innerHTML = excusa;
+  console.log("Look Mom! I'm on the internet!");
+}
+
+document.getElementById("boton").onclick = function() {
+  generar();
 };
